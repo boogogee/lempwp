febfeef3717a88518471072cdba36dc36ebe5655
Cody McDonald <cmcdonald@linode.com>
2018-06-29 15:58:30 -0400

    MariaDB

--- lempwp.sh
+++ lempwp.sh
@@ -2,9 +2,12 @@
 
 # Set Variables
 IP=$(curl -4 -s icanhazip.com)
-MYSQLROOTPASS="$(openssl rand -base64 12)"
-PASSWDDB="$(openssl rand -base64 12)"
+MYSQLROOTPASS="$(openssl rand -hex 24)"
+PASSWDDB="$(openssl rand -hex 24)"
 
+clear
+if [[ ! $WPUSER ]]; then read -p "Wordpress username?" WPUSER; fi
+if [[ ! $DBNAME ]]; then read -p "Wordpress DB name?" DBNAME; fi
 # Display random generated passwords 
 clear
 echo -e "\e[31mPlease enter the following information into a password manager.  You will only see it once!\e[0m"
@@ -12,8 +15,14 @@ echo
 echo -e "\e[32mMySQL Root Password\e[0m"
 echo $MYSQLROOTPASS
 echo 
-echo -e "\e[32mWordpress DB Password\e[0m"
+echo -e "\e[32mWordpress DB Username\e[0m"
+echo $WPUSER
+echo
+echo -e "\e[32mWordpress DB Username Password\e[0m"
 echo $PASSWDDB
+echo
+echo -e "\e[32mWordpress DB Name\e[0m"
+echo $DBNAME
 echo
 read -p "Press [Enter] key to continue"
 
@@ -24,12 +33,44 @@ yum -y install nginx
 yum -y install wget
 yum -y install php php-mysql php-fpm
 yum -y install mariadb-server mariadb
+yum -y install expect
 
 # Start services and set them to start on boot
 systemctl start nginx
 systemctl enable nginx
 systemctl start mariadb
 systemctl enable mariadb
+
+# Setup mariadb
+SECURE_MYSQL=$(expect -c "
+set timeout 3
+spawn mysql_secure_installation
+expect \"Enter current password for root (enter for none):\"
+send \"$NONE\r\"
+expect \"root password?\"
+send \"y\r\"
+expect \"New password:\"
+send \"$MYSQLROOTPASS\r\"
+expect \"Re-enter new password:\"
+send \"$MYSQLROOTPASS\r\"
+expect \"Remove anonymous users?\"
+send \"y\r\"
+expect \"Disallow root login remotely?\"
+send \"y\r\"
+expect \"Remove test database and access to it?\"
+send \"y\r\"
+expect \"Reload privilege tables now?\"
+send \"y\r\"
+expect eof
+")
+
+echo "$SECURE_MYSQL"
+
+mysql -uroot -p${MYSQLROOTPASS} -e "CREATE DATABASE ${DBNAME};"
+mysql -uroot -p${MYSQLROOTPASS} -e "CREATE USER ${WPUSER}@localhost;"
+mysql -uroot -p${MYSQLROOTPASS} -e "SET PASSWORD FOR '${WPUSER}'@'localhost' = PASSWORD('$PASSWDDB');"
+mysql -uroot -p${MYSQLROOTPASS} -e "GRANT ALL PRIVILEGES ON ${DBNAME}.* TO ${WPUSER}@localhost IDENTIFIED BY '${PASSWDDB}';"
+mysql -uroot -p${MYSQLROOTPASS} -e "FLUSH PRIVILEGES;"
 
 # Open port 80 
 firewall-cmd --zone=public --add-port=80/tcp --permanent
@@ -57,6 +98,12 @@ mv /usr/share/nginx/html/wordpress/* /usr/share/nginx/html
 # Set ownership and permissions
 sudo chown -R nginx:nginx /usr/share/nginx/html/
 sudo chmod -R 755 /usr/share/nginx/html/
+
+#wp-config.php 
+mv /usr/share/nginx/html/wp-config-sample.php /usr/share/nginx/html/wp-config.php
+sed -i -e "s/database_name_here/${DBNAME}/" /usr/share/nginx/html/wp-config.php
+sed -i -e "s/username_here/$WPUSER/" /usr/share/nginx/html/wp-config.php
+sed -i -e "s/password_here/$PASSWDDB/" /usr/share/nginx/html/wp-config.php
 
 # Copy nginx conf to /etc/nginx/conf.d/ 
 cp /root/default.conf /etc/nginx/conf.d/ 
